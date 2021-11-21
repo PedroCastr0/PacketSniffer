98df730bddf3d2d652556773c8ef5d6862a6f913
PedroCastr0 <pedrocastros027@gmail.com>
2021-11-20 22:14:05 -0300

    Resolvido problema ARP

--- src/recepcao_socket.c
+++ src/recepcao_socket.c
@@ -28,7 +28,7 @@
 
 unsigned char buff1[BUFFSIZE]; // buffer de recepcao
 
-typedef struct{
+typedef struct {
     int packet_count;
     float ipv4, arp, ipv6;
 
@@ -47,22 +47,18 @@ int on;
 struct ifreq ifr;
 
 // Baseado no livro "Hacking the art of exploitation"
-void dump(const unsigned char *data_buffer, const unsigned int length)
-{
+void dump(const unsigned char *data_buffer, const unsigned int length) {
     unsigned char byte;
     unsigned int i, j;
 
-    for (i = 0; i < length; i++)
-    {
+    for (i = 0; i < length; i++) {
         byte = data_buffer[i];
         printf("%02x ", data_buffer[i]);
-        if (((i % 16) == 15) || (i == length - 1))
-        {
+        if (((i % 16) == 15) || (i == length - 1)) {
             for (j = 0; j < 15 - (i % 16); j++)
                 printf("   ");
             printf("| ");
-            for (j = (i - (i % 16)); j <= i; j++)
-            {
+            for (j = (i - (i % 16)); j <= i; j++) {
                 byte = data_buffer[j];
                 if ((byte > 31) && (byte < 127))
                     printf("%c", byte);
@@ -74,22 +70,19 @@ void dump(const unsigned char *data_buffer, const unsigned int length)
     }
 }
 
-void intHandler(int dummy)
-{
-    printf("#########################################\n");
+void intHandler(int dummy) {
+    printf("#############################################\n");
     printf("\t\tESTATISTICAS\n\nTotal de pacotes = %d\nPacotes IPV4 = %.2f%%\nPacotes IPV6 = %.2f%%\nPacotes ARP = %.2f%%\n", p.packet_count, (p.ipv4 / p.packet_count) * 100, (p.ipv6 / p.packet_count) * 100, (p.arp / p.packet_count) * 100);
     exit(0);
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char **argv) {
     int recv_length;
     signal(SIGINT, intHandler);
     /* Criacao do socket. Todos os pacotes devem ser construidos a partir do protocolo Ethernet. */
     /* De um "man" para ver os parametros.*/
     /* htons: converte um short (2-byte) integer para standard network byte order. */
-    if ((sockd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) < 0)
-    {
+    if ((sockd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) < 0) {
         printf("Erro na criacao do socket.\n");
         exit(1);
     }
@@ -105,29 +98,24 @@ int main(int argc, char *argv[])
     ioctl(sockd, SIOCSIFFLAGS, &ifr);
 
     // recepcao de pacotes
-    while (1)
-    {
-
+    while (1) {
         recv_length = recv(sockd, (char *)&buff1, sizeof(buff1), 0x0);
         printf("MAC Destino: %x:%x:%x:%x:%x:%x \n", buff1[0], buff1[1], buff1[2], buff1[3], buff1[4], buff1[5]);
         printf("MAC Origem:  %x:%x:%x:%x:%x:%x \n", buff1[6], buff1[7], buff1[8], buff1[9], buff1[10], buff1[11]);
-        //printf("Ether type: %x%x\n", buff1[12], buff1[13]);
+        printf("Ether type: 0%x%x\n", buff1[12], buff1[13]);
         printf("\n");
 
         sprintf(type, "0%x%x", buff1[12], buff1[13]);
 
-        if (strcmp(type, "080") == 0)
-        {
+        if (strcmp(type, "080") == 0) {
             p.ipv4++;
             dump(buff1, recv_length);
         }
-        else if (strcmp(type, "086dd") == 0)
-        {
+        else if (strcmp(type, "086dd") == 0) {
             p.ipv6++;
             dump(buff1, recv_length);
         }
-        else if (strcmp(type, "0806") == 0)
-        {
+        else if (strcmp(type, "086") == 0) {
             p.arp++;
             dump(buff1, recv_length);
         }
